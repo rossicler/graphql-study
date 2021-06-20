86483806ec8a785a9a02d9180a553255b91b3c36
Rossicler Júnior <rossiclerjr@gmail.com>
2021-06-20 02:05:34 -0300

    Added graphql server with NodeJS

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+
+const schema = require("./schema/schema");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://user:test123@cluster0.rweym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening for requests on port 4000");
+});
